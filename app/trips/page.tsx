e6c02d09b35392c17638dfff142746b611cb4471
ROYPaul18 <114011395+ROYPaul18@users.noreply.github.com>
2025-10-13 11:40:43 +0200

    Ajout Globe Page

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -7,12 +7,18 @@ import { prisma } from "@/lib/prisma";
 export default async function TripsPage() {
     const session = await auth()
     const trips = await prisma.trip.findMany({
-        where: {userId: session?.user?.id}
+        where: { userId: session?.user?.id }
     })
-
+    const sortedTrips = [...trips].sort((a, b) => new Date(b.startDate).getTime() - new Date(a.startDate).getTime())
     if (!session) {
         return <div className="flex justify-center items-center h-screen text-gray-700 text-xl"> Please Sign In.</div>
     }
+
+    const today = new Date()
+    today.setHours(0, 0, 0, 0);
+    const upcommingsTrips = sortedTrips.filter(
+        (trip) => new Date(trip.startDate) >= today
+    );
 
     return <div className="space-y-6 container mx-auto px-4 py-8">
         <div className="flex items-center justify-between">
@@ -26,11 +32,57 @@ export default async function TripsPage() {
                 <CardTitle> Welcome back, {session.user?.name} </CardTitle>
             </CardHeader>
             <CardContent>
-                <p> 
-                    {trips.length === 0 ? "Start planning your first trip" : `You have ${trips.length} ${trips.length === 1 ? "trip" : "trips planned"}`}
+                <p>
+                    {trips.length === 0
+                        ? "Start planning your first trip"
+                        : `You have ${trips.length} 
+                    ${trips.length === 1 ? "trip" : "trips"
+                        } planned. ${upcommingsTrips.length > 0
+                            ? `${upcommingsTrips.length} upcoming.`
+                            : ""
+                        }   `}
                 </p>
             </CardContent>
         </Card>
+        <div>
+            <h2 className="text-xl font-semibold mb-4"> Your Recent Trips</h2>
+            {trips.length === 0 ? (
+                <Card>
+                    <CardContent className="flex flex-col items-center justify-center py-8 ">
+                        <h3 className="text-xl font-medium mb-2"> No trips yet.</h3>
+                        <p className="text-center mb-4 max-w-md">
+                            Start planning your adventure by creating your first trip.
+                        </p>
+                        <Link href={"/trips/new"}>
+                            <Button> Create trips</Button>
+                        </Link>
+                    </CardContent>
+                </Card>
+
+            )
+                : <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {sortedTrips.slice(0, 6).map((trip, key) => (
+                        <Link key={key} href={`trips/${trip.id}`}>
+                            <Card className=" h-full hover:shadow-md transition-shadow">
+                                <CardHeader>
+                                    <CardTitle className="line-clamp-1">
+                                        {trip.title}
+                                    </CardTitle>
+                                </CardHeader>
+                                <CardContent>
+                                    <p className="text-sm line-clamp-2 mb-2">
+                                        {trip.description}
+                                    </p>
+                                    <div className="text-sm">
+                                        {new Date(trip.startDate).toLocaleDateString()} - {new Date(trip.endDate).toLocaleDateString() }
+                                    </div>
+                                </CardContent>
+                            </Card>
+                        </Link>
+                    ))}
+                </div>}
+        </div>
+
 
     </div>
 }
