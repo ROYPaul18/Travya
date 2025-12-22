af37b5984a3a9e6a516db2cdb9bdb45673cf841b
ROYPaul18 <114011395+ROYPaul18@users.noreply.github.com>
2025-12-22 20:29:24 +0100

    prisma update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import 'dotenv/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
 
-const globalFormPrisma = globalThis as unknown as {prisma: PrismaClient};
-export const prisma = globalFormPrisma.prisma || new PrismaClient();
-if(process.env.NODE_ENV !== "production") globalFormPrisma.prisma = prisma
+const connectionString = process.env.DATABASE_URL!;
+
+const adapter = new PrismaPg({
+  connectionString
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
