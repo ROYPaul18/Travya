b2c82a1104d4bc7d808cde5a5e9506bd85eeb553
ROYPaul18 <114011395+ROYPaul18@users.noreply.github.com>
2025-12-22 20:33:11 +0100

    corection 1

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,20 @@
 import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma/client';
+import { PrismaClient } from '@/generated/prisma/client'; 
 
 const connectionString = process.env.DATABASE_URL!;
+const adapter = new PrismaPg({ connectionString });
 
-const adapter = new PrismaPg({
-  connectionString
-});
+const prismaClientSingleton = () => {
+  return new PrismaClient({ adapter });
+};
 
-const prisma = new PrismaClient({ adapter });
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
 
 export { prisma };
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
